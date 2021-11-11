7cad05f5cfbf6d21884b7e7061b695eec02c5684
m-yangyu <2388560359@qq.com>
2021-11-11 23:16:46 +0800

    feat: 简易实现将vue文件转换成js内容

--- packages/fmp-cli/lib/middleware/useVue.ts
+++ packages/fmp-cli/lib/middleware/useVue.ts
@@ -1,8 +1,6 @@
 import { readStream } from '../utils/index';
 import * as compiler  from '@vue/compiler-sfc';
-import { baseCompile } from '@vue/compiler-core';
-import { cssInDomStyle } from '../parser';
-// import { vueParser } from "lib/parser";
+import { vueParser } from "../parser";
 
 export const useVue = async (ctx, next) => {
     await next();
@@ -13,16 +11,16 @@ export const useVue = async (ctx, next) => {
             filename: context.filename,
             sourceMap: false,
         })
-        await cssInDomStyle(descriptor);
-        const { code } = baseCompile(descriptor.template?.content as string);
-        const render = new Function(code);
+        await vueParser(descriptor);
+        // const { code } = baseCompile(descriptor.template?.content as string);
+        // const render = new Function(code);
         
         // todo: 解析js成ast， 获取他的import引入的组件
         // todo：render的方法写入到js的export的options当中
         // todo：所有的组件改为同一个组件模板，添加name参数做组件的区分
 
 
-        console.log(descriptor);
+        // console.log(descriptor);
         ctx.vueDescriptor = descriptor;
     }
 }
--- packages/fmp-cli/lib/parser/vueParser.ts
+++ packages/fmp-cli/lib/parser/vueParser.ts
@@ -1,3 +1,34 @@
-export const vueParser = async () => {
-    
+import { baseCompile, baseParse } from '@vue/compiler-core';
+import { cssInDomStyle } from '../parser';
+// import { init, parse } from 'es-module-lexer';
+
+export const vueParser = async (descriptor) => {
+    // await init;
+    await cssInDomStyle(descriptor);
+
+    // 先放着不动，后续在用，获取到模块的引用路径
+    // const scriptContent = descriptor.script.content;
+    // const [ imports ] = parse(scriptContent);
+    // const importComponents: string[] = [];
+    // imports.forEach((imp) => {
+    //     importComponents.push(scriptContent.substring(imp.ss + 6, imp.s - 6).trim());
+    // });
+    // const ast = baseParse(descriptor.template?.content as string);
+
+    const { code } = baseCompile(descriptor.template?.content as string);
+
+    const scriptContentArr = descriptor.script.content.split('\n');
+
+    let exportDefaultIndex = 0;
+
+    for (let i = 0; i < scriptContentArr.length; i++) {
+        if (/export default/.test(scriptContentArr[i])) {
+            exportDefaultIndex = i;
+            break;
+        }
+    }
+
+    scriptContentArr.splice(exportDefaultIndex + 1, 0, `render: (new Function(${code})()),`);
+
+    descriptor.script.content = scriptContentArr.join('\n');
 }
